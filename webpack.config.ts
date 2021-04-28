e6129860476ebeb4413145499eb821a41965b0e2
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2021-04-28 23:19:05 +0300

    Revert "webpack and tsconfig are updated"
    This reverts commit 1680abaf63c3937a3e6b4cdd9077990e4c4a8e27.

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import webpack from "webpack";
 import { Configuration as WebpackConfig } from "webpack";
 import { Configuration as WebpackDevConfig } from "webpack-dev-server";
-import TerserPlugin from 'terser-webpack-plugin';
 
 interface Config extends WebpackConfig {
   devServer?: WebpackDevConfig;
@@ -51,9 +50,6 @@ const config: Config = {
     // hotOnly: false,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
-  optimization: {
-    minimizer: [new TerserPlugin()],
-  },
 };
 
 export default config;
